e3cfb52565ba704a1ee7fcbb73b438331543f1fc
Quebec-Eric <quebec.ericjs@gmail.com>
2022-03-18 07:31:24 -0300

    Lista

--- Grafos/Lista1/Grafo.c
+++ Grafos/Lista1/Grafo.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "Grafo.h"
 
+///inicio struc grafo
 struct grafo
 {
     int ponderado;
@@ -12,24 +13,34 @@ struct grafo
     float **pesos;
     int *grau;
 };
+//fim struct grafo
 
+
+//Incio criarGrafo
+//Passagem de parametro int int int
+//Return ponteiro de grafico
 Grafo *criarGrafo(int vertice, int grau, int ponderado)
 {
 
     Grafo *novoGrafico;
+    
     novoGrafico = (Grafo *)malloc(sizeof(struct grafo));
     if (novoGrafico != NULL)
     {
+        //vicular variaves 
         novoGrafico->vertices = vertice;
         novoGrafico->grauMaximo = grau;
         novoGrafico->ponderado = ponderado;
+        
+        //criacao da coluna na memora com o tamanho senndo referente ao grau maximo
         novoGrafico->grau = (int *)calloc(vertice, sizeof(int *)); // usei calloc pois nao quero lixo e so quero 0
         if (novoGrafico->grau != NULL)
         {
+             //criacao da linhas referete a coluna na memoria
             novoGrafico->arestas = (int **)malloc(vertice * sizeof(int *));
             if (novoGrafico->arestas != NULL)
             {
-
+                // se o grafo for ponderado criamos uma sub lista na memoria onde iremos vencular esse peso com a aresta
                 for (int i = 0; i < novoGrafico->vertices; i++)
                 {
                     novoGrafico->arestas[i] = (int *)malloc(vertice * sizeof(int *));
@@ -66,7 +77,13 @@ Grafo *criarGrafo(int vertice, int grau, int ponderado)
 
     return novoGrafico;
 }
+//fim criacaoDoGRafo
 
+
+
+//Inicio colocaValores
+//passagem de paragemto ponteiro de GRafo , int , int , int , float
+//return it para ser utilizado como confirmacao se foi ou nao possivel incluir o valor 
 int colocarValores(Grafo *grafo, int vertice1, int verticd2, int retornoE, float peso)
 {
     if (grafo == NULL)
@@ -76,9 +93,10 @@ int colocarValores(Grafo *grafo, int vertice1, int verticd2, int retornoE, float
     if (verticd2 < 0 || verticd2 >= grafo->vertices)
         return 0;
 
+    //alocando os valores na eferencia onde o vertice1 se referencia     
     grafo->arestas[vertice1][grafo->grau[vertice1]] = verticd2;
     if (grafo->ponderado)
-        grafo->pesos[vertice1][grafo->grau[vertice1]] = peso;
+        grafo->pesos[vertice1][grafo->grau[vertice1]] = peso;//se for ponderado colocarmos o peso em uma lista subjacete
     grafo->grau[vertice1]++;
 
     if (retornoE == 1)
@@ -86,49 +104,68 @@ int colocarValores(Grafo *grafo, int vertice1, int verticd2, int retornoE, float
     return 1;
 }
 
+//Inicio imprimeGrafoLista
+//Passagem de paragemto ponteiro de Grafo
+//return Void
 void imprimeGrafoLista(Grafo *grafico)
 {
     if (grafico == NULL)
     {
         return;
     }
-
+    //colocar os valores de referencias que estao sendo referenciados pelo vertice 
     for (int i = 0; i < grafico->vertices; i++)
     {
         printf("%d: ", i);
+        //acessar os valores no locar adjacete 
         for (int j = 0; j < grafico->grau[i]; j++)
         {
+            // se for o valor for ponderado iresmos colocar o primeiro valor da arestas  e depois com o peso de cada local
             if (grafico->ponderado)
                 printf("V[%d] Peso(%.2f), ", grafico->arestas[i][j], grafico->pesos[i][j]);
             else
-                printf("[%d], ", grafico->arestas[i][j]);
+                printf("[%d], ", grafico->arestas[i][j]);// se nao for colocar somete os valores da primeira lista de adjacencia
                  //printf("--> ");
         }
         printf("\n");
     }
 }
+//fim ImprimeGRafoLista
 
+
+
+//Inicio imprimeGrafomatriz 
+//passagem de parametro ponteiro de Grafo
+// return Void
 void imprimeGrafomatriz(Grafo *grafico)
 {
     if (grafico == NULL)
     {
         return;
     }
+
     printf("   ");
+    //criar uma imagem ilustrativa
     for (int i = 0; i < grafico->vertices; i = i + 1)
     {
          
         printf("%d\t", i);
     }
     printf("\n");
+
+    //Inicio acessar os valores
     for (int i = 0; i < grafico->vertices; i = i + 1)
     {
         printf("%d: ", i);
+        
+        //inicio tratar os dados da lista para colocar na matriz
         int *arr = bolha(grafico->arestas[i], grafico->grau[i], 5);
 
+         //acessar os dados do vetor   
         for (int j = 0; j < grafico->vertices; j = j + 1)
         {
             //  printf("%d\t", arr[j]);
+            
             if (arr[j] == j && arr[j] != 0)
             {
                 // printf("%d\t", arr[j]);
@@ -143,7 +180,12 @@ void imprimeGrafomatriz(Grafo *grafico)
         printf("\n");
     }
 }
+// Fim imprimeGrafomatriz
 
+
+//Inicio Bolha -Tratamento da lista adjacete
+//parametros array , int , int
+//retur poteiro d array
 int *bolha(int *arrayDesordenado, int tamanho, int tamanhoGeral)
 {
     int valor = 0;
@@ -166,7 +208,11 @@ int *bolha(int *arrayDesordenado, int tamanho, int tamanhoGeral)
     }
     return arrayDesordenado;
 }
+//Fim bolha
 
+//Iicio coleta Lixo
+//passgme de parametros  ponteirp deGrafico
+//retur Void 
 void coletarLixo(Grafo *grafico)
 {
     if (grafico != NULL)
@@ -194,4 +240,6 @@ void coletarLixo(Grafo *grafico)
             printf("Grafico desalocado da memoria");
         }
     }
+
 }
+//fim ColetaLixo
--- Grafos/Lista1/main.c
+++ Grafos/Lista1/main.c
@@ -7,21 +7,37 @@ Grafo *criacaoDoGRafo();
 int inserirNoGrafo(Grafo *grafico);
 
 int ponderado = 0;
+
+
+//Inicio  int mais 
 int main()
 {
+ //demostracao de como deve ser a entrada do programa
   demonstracao();
-  Grafo *grafo = criacaoDoGRafo();
-  int retornarVerdade = inserirNoGrafo(grafo);
+  
+   
+  Grafo *grafo = criacaoDoGRafo();//criacao da alocacao dinamica do nosso Grafo
+
+
+  int retornarVerdade = inserirNoGrafo(grafo);//Fazer a isercao das aretas no rafico
+  
+  //vericiar erro
   if (retornarVerdade != 1)
   {
     printf("\nErro ao incluir valor\n");
   }
-
+ 
+ //desalocar lixo da memorai
   coletarLixo(grafo);
 
   return 0;
 }
+//fim int mai
 
+
+//inicio demostacao
+//Passgem de parametro Void
+//retur void
 void demonstracao()
 {
 
@@ -30,7 +46,12 @@ void demonstracao()
   printf("Entrada para a criacao de nosso grafo == \n");
   printf("Entre  se ele e ponderado ou nao \n\n");
 }
+//fim demostracao
 
+
+//Inicio fucao criacaoDoGrafico
+//Passagem de parametro Void
+//Return um ponteiro de Grafo 
 Grafo *criacaoDoGRafo()
 {
   int ponderado = 0;
@@ -40,10 +61,15 @@ Grafo *criacaoDoGRafo()
     printf("ERRO");
     return NULL;
   }
-  Grafo *novoGrafo = criarGrafo(5, 5, ponderado);
+  Grafo *novoGrafo = criarGrafo(5, 5, ponderado);//Criar o grafico com o numero 4 de vertices , com o grau maximo 4
   return novoGrafo;
 }
+//fim criacaoDoGrafo
 
+
+//Iicio iserirNografo/Mostraar
+//Passagem de parametro Um ponteiro de Grafico
+//return um valor Int
 int inserirNoGrafo(Grafo *grafico)
 {
   int saber = 0;
@@ -55,55 +81,55 @@ int inserirNoGrafo(Grafo *grafico)
   scanf("%d", &saber);
   if (saber == 0)
   {
+    //Inicio Incerssao grafo  direcioado e nao ponderado
     colocarValores(grafico, 0, 1, 0, 0);
     colocarValores(grafico, 1, 3, 0, 0);
     colocarValores(grafico, 1, 2, 0, 0);
     colocarValores(grafico, 2, 4, 0, 0);
     colocarValores(grafico, 3, 4, 0, 0);
-    saber = colocarValores(grafico, 4, 1, 0, 0);
-    imprimeGrafomatriz(grafico);
-  }
+    saber = colocarValores(grafico, 4, 1, 0, 0); // retornar saber para saber se foi possivel incluir
+    imprimeGrafomatriz(grafico);// primtar na tela a matriz do grafo
+  } 
   else if (saber == 2)
   {
+    //Inicio grafo nao direcionado e ponderado
     colocarValores(grafico, 0, 1, 0, 10);
     colocarValores(grafico, 1, 0, 0, 10);
-
     colocarValores(grafico, 1, 3, 0, 6);
     colocarValores(grafico, 3, 1, 0, 6);
-
     colocarValores(grafico, 1, 2, 0, 5);
     colocarValores(grafico, 2, 1, 0, 5);
-
     colocarValores(grafico, 2, 4, 0, 11);
     colocarValores(grafico, 4, 2, 0, 11);
-
     colocarValores(grafico, 3, 4, 0, 2.5);
     colocarValores(grafico, 4, 3, 0, 2.5);
-
     colocarValores(grafico, 4, 1, 0, 4);
-    saber = colocarValores(grafico, 1, 4, 0, 4);
-    imprimeGrafoLista(grafico);
+    saber = colocarValores(grafico, 1, 4, 0, 4);//retur de confirmacao
+    imprimeGrafoLista(grafico);//pritar na tela a lista de adjacecia
   }
   else if (saber == 1)
   {
+    //Inciio Grafo não-direcionado não-ponderado
     colocarValores(grafico, 0, 1, 0, 0);
     colocarValores(grafico, 1, 3, 0, 0);
     colocarValores(grafico, 1, 2, 0, 0);
     colocarValores(grafico, 2, 4, 0, 0);
     colocarValores(grafico, 3, 4, 0, 0);
-    saber = colocarValores(grafico, 4, 1, 0, 0);
-    imprimeGrafomatriz(grafico);
+    saber = colocarValores(grafico, 4, 1, 0, 0);//retur de confirmacao
+    imprimeGrafoLista(grafico);//imprimir a lista a tela
   }
   else
   {
+    //Grafo direcionado ponderado
     colocarValores(grafico, 0, 1, 0, 10);
     colocarValores(grafico, 1, 3, 0, 6);
     colocarValores(grafico, 1, 2, 0, 5);
     colocarValores(grafico, 2, 4, 0, 11);
     colocarValores(grafico, 3, 4, 0, 2.5);
     saber = colocarValores(grafico, 4, 1, 0, 4);
-    imprimeGrafoLista(grafico);
+    imprimeGrafoLista(grafico);//imprimir lista na tela 
   }
 
   return saber;
 }
+//Fim iserirNografo/Mostraars
